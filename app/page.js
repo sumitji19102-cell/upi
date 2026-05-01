ae4f5e0b19cfcca67fb82a6fdcd949bf6341ea6c
sumitji19102-cell <sumitji19102@gmail.com>
2026-05-01 06:33:49 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -64,7 +64,7 @@ export default function Page() {
       </div>
 
       {/* CARD */}
-      <div className="mt-3 bg-[#121212] rounded-[14px] p-3 px-4 mx-2 ">
+      <div className="mt-3 bg-[#212626] rounded-[14px] p-3 px-4 mx-2 ">
 
         <div className="text-[15px] font-bold">Paid to</div>
 
